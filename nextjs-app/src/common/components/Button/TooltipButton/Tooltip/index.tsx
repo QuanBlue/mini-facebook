f808d0228590b34f8dae4ad16247013111adb29f
Nguyen Thanh Quan <quannguyenthanh558@gmail.com>
2023-11-29 10:11:59 +0700

    fix (UI): wrap tooltip

--- nextjs-app/src/common/components/Button/TooltipButton/Tooltip/index.tsx
+++ nextjs-app/src/common/components/Button/TooltipButton/Tooltip/index.tsx
@@ -11,7 +11,9 @@ function Tooltip({ describe, className }: Props) {
          className={`bg-black absolute -bottom-9 rounded-lg opacity-70  ${className}`}
       >
          <div className="px-3 py-2">
-            <p className="text-[12px] text-white">{describe}</p>
+            <p className="whitespace-nowrap text-[12px] text-white">
+               {describe}
+            </p>
          </div>
       </div>
    );
